699bd6f2aefc52b05ceb57a6e3bd88e5cd15eaae
JoseRobson2301 <111148505+JoseRobson2301@users.noreply.github.com>
2023-01-17 15:01:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ function checkPassword() {
     alert("Senha incorreta, tente novamente.");
   }
 }
+
+function checkAuth() {
+  // Verifica se o usuário está autenticado
+  if (!isAuthenticated()) {
+    document.getElementById("lockScreen").style.display = "block";
+    document.getElementById("appContent").style.display = "none";
+  } else {
+    document.getElementById("lockScreen").style.display = "none";
+    document.getElementById("appContent").style.display = "block";
+  }
+}
+
+function unlock() {
+  const password = document.getElementById("password").value;
+  if (password === "minhasenha") {
+    document.getElementById("lockScreen").style.display = "none";
+    document.getElementById("appContent").style.display = "block";
+  } else {
+    alert("Senha incorreta, tente novamente.");
+  }
+}
