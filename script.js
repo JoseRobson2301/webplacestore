bfc510fcbea1aaaa1d268b0ddf3b1a82df26e536
JoseRobson2301 <111148505+JoseRobson2301@users.noreply.github.com>
2023-01-17 15:10:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-(document).ready(function(){
-  $("button-enviar").click(function(){
-    $("button-enviar").animate({ 
-      backgroundColor:"blue"
-    },1000);
-  });
-});
-
-//function checkPassword() {
-//  const password = document.getElementById("password").value;
-//  if (password === "minhasenha") {
-//    alert("Senha correta!");
-//  } else {
-//    alert("Senha incorreta, tente novamente.");
-//  }
-//}
-
 function checkAuth() {
   // Verifica se o usuário está autenticado
   if (!isAuthenticated()) {
