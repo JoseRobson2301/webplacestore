ef8a34a7985f65d0ef8e217713e380d1bfe66b0f
JoseRobson2301 <111148505+JoseRobson2301@users.noreply.github.com>
2023-01-17 14:53:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,12 @@
     },1000);
   });
 });
+
+function checkPassword() {
+  const password = document.getElementById("password").value;
+  if (password === "minhasenha") {
+    alert("Senha correta!");
+  } else {
+    alert("Senha incorreta, tente novamente.");
+  }
+}
