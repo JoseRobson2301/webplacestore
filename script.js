b71fe007709902029bb983a9ceb957f87ca18193
JoseRobson2301 <111148505+JoseRobson2301@users.noreply.github.com>
2023-01-17 14:18:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(button-enviar).ready(function(){
   $("main section#contato form input").click(function(){
     $("main section#contato form input").animate({ 
       backgroundColor: "blue"
