dd11975da13cd92e8677c55f87bc115cd00f09f7
JoseRobson2301 <111148505+JoseRobson2301@users.noreply.github.com>
2023-01-17 14:36:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-  $("main section#contato form input").click(function(){
-    $("main section#contato form input").animate({ 
-      backgroundColor: "blue"
-    }, 1000);
+  $("button-enviar").click(function(){
+    $("button-enviar").animate({ 
+      backgroundColor:"blue"
+    },1000);
   });
 });
