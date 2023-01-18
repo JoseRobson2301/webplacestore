263aba06890ad1098c7fdfa98209a15aec81d50b
JoseRobson2301 <111148505+JoseRobson2301@users.noreply.github.com>
2023-01-18 08:16:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const lockScreen = document.getElementById("lockScreen");
 const lockBtn = document.getElementById("lockBtn");
 const unlockBtn = document.getElementById("unlockBtn");
 
 lockBtn.addEventListener("click", function() {
-  lockScreen.classList.remove("hidden");
+  lockScreen.classList.add("visible");
 });
 
 unlockBtn.addEventListener("click", function() {
-  lockScreen.classList.add("hidden");
+  lockScreen.classList.remove("visible");
 });
